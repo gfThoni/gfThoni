df0790a975b8b727dd05a2f4adad659a2599df20
gfThoni <126793534+gfThoni@users.noreply.github.com>
2024-05-10 20:27:19 -0300

    Update and rename README.md to Projeto.py

---
+++ Projeto.py
@@ -0,0 +1,6 @@
+— 👋 Oi, estou @gfThoni
+- 👀 Estou interessado na linguagem Python para jogos(e sem pygame)😅.
+- 🌱 Atualmente estou aprendendo python                                                    
+- 📫 Meu email é gf.thoni@gmail.com
+
+<!--- gfThoni/gfThoni é um ✨ repositório especial ✨ porque seu 'README.md' (este arquivo) aparece no seu perfil do GitHub. Você pode clicar no link Visualizar para ver suas alterações. --->
